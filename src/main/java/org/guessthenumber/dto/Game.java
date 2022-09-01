fa13daed9b490f0d8c46a3d7f3342dc868ace883
alex-merecka <amerecka@webcreek.com>
2022-09-01 13:50:21 -0500

    Setup Game and Round models.

--- src/main/java/org/guessthenumber/dto/Game.java
+++ src/main/java/org/guessthenumber/dto/Game.java
@@ -1,4 +1,34 @@
 package org.guessthenumber.dto;
 
 public class Game {
+
+    private int gameId;
+
+    private String gameAnswer;
+
+    private String gameStatus;
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getGameAnswer() {
+        return gameAnswer;
+    }
+
+    public void setGameAnswer(String gameAnswer) {
+        this.gameAnswer = gameAnswer;
+    }
+
+    public String getGameStatus() {
+        return gameStatus;
+    }
+
+    public void setGameStatus(String gameStatus) {
+        this.gameStatus = gameStatus;
+    }
 }
--- src/main/java/org/guessthenumber/dto/Round.java
+++ src/main/java/org/guessthenumber/dto/Round.java
@@ -1,4 +1,46 @@
 package org.guessthenumber.dto;
 
+import java.time.LocalDateTime;
+
 public class Round {
+
+    private int roundId;
+
+    private String guess;
+
+    private LocalDateTime guessTime;
+
+    private String roundResult;
+
+    public int getRoundId() {
+        return roundId;
+    }
+
+    public void setRoundId(int roundId) {
+        this.roundId = roundId;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
+
+    public LocalDateTime getGuessTime() {
+        return guessTime;
+    }
+
+    public void setGuessTime(LocalDateTime guessTime) {
+        this.guessTime = guessTime;
+    }
+
+    public String getRoundResult() {
+        return roundResult;
+    }
+
+    public void setRoundResult(String roundResult) {
+        this.roundResult = roundResult;
+    }
 }
