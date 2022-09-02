b39698f0e37214a0b8c31957711f10c154f3d4e2
alex-merecka <amerecka@webcreek.com>
2022-09-01 23:16:39 -0500

    New games are being persisted to the database.

--- SQL/guessthenumber-db.sql
+++ SQL/guessthenumber-db.sql
@@ -4,21 +4,15 @@ CREATE DATABASE guessthenumberdb;
 USE guessthenumberdb;
 
 CREATE TABLE game(
-gameid INT PRIMARY KEY AUTO_INCREMENT,
-`name` VARCHAR(50) NOT NULL,
-`description` VARCHAR(100)
-);
-
-CREATE TABLE meeting(
 gameId INT PRIMARY KEY AUTO_INCREMENT,
 gameAnswer VARCHAR(4) NOT NULL,
-gameStatus VARCHAR(15) NOT NULL;
+gameStatus VARCHAR(15) NOT NULL);
 
 CREATE TABLE round(
 roundId INT PRIMARY KEY AUTO_INCREMENT,
 guess VARCHAR(4) NOT NULL,
-guessTime TIMESTAMP NOT NULL),
-roundResult VARCHAR(10);
+guessTime TIMESTAMP NOT NULL,
+roundResult VARCHAR(10) NOT NULL);
 
 CREATE TABLE game_round(
 gameId INT NOT NULL,
